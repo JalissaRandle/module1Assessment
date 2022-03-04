d37430b8391b14f91087a1e8ede46fb7188d9e00
JalissaRandle <Jalissar18@Gmail.com>
2022-03-04 13:49:34 -0600

    final final

--- pasword.js
+++ pasword.js
@@ -5,15 +5,26 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-console.log("Welcome to the password validator tool")
-reader.question("Validate your password here:", function(input){
-    tokens = input.split('');
-    if (input >= 10){
-        console.log("Success")
-    } else {
-        console.log("failure")
-    }
-});
+reader.question('enter your password here', function(password)(
+
+if(password.legnhth >= 10){
+    console.log("pasword meets the length requiremnt")
+}else(password.length < 10){
+    console.log('password is too short')
+})
+
+reader.close()
+
+
+// console.log("Welcome to the password validator tool")
+// reader.question("Validate your password here:", function(input){
+//     tokens = input.split('');
+//     if (input >= 10){
+//         console.log("Success")
+//     } else {
+//         console.log("failure")
+//     }
+// });
 
 
 
